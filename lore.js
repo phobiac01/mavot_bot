0973dafa88c39c0e41af01dedace90fe14057399
Phobiac01 <phobiac01@gmail.com>
2020-11-19 19:23:39 -0700

    Restructured and updated lore page Added media directory

--- lore.js
+++ lore.js
@@ -1,18 +1,127 @@
 
 module.exports = {
-    main : {
-        introduction : "The DragonScape is a worldbuilding project that I have been working and drawing on for 3 years now, with The help of some awesome folks This wiki has been made possible and will serve as a wiki for consolidating the years of work, lore, and ideas for the setting. The DragonScape is a fantasy setting in which the Americas are beset by billions of draconic gods, the wildernesses of the American Continents are enchanted, the wildlife turned draconic, and the humans that once lived in the Americas were changed to the drekir. Awakening long after the pulse, the once human drekir are left to survive in a world that is both familiar and strange. From strange tribes growing from the foundations of initial survivors trying to understand where the world they knew went, to the grand realms of the sivilāo and the majesty of the Balār to the strange warping of the magical landscape around them. The DragonScape is full of many different stories, people, cultures and societies. Feel free to read on to find more!",
+    lore : {
+        overview : "The Dragonscape is a fantasy setting where draconic gods warp the americas and the people within it into a draconic fantasy. Animals are turned to dragnos, forests enchanted, and the humans turned to drekir. Now left in the Mystical forests of the Americas to survive the once human drekir must start from scratch in a new world.",
+        subcategories : [
+            {
+                name : "The Pulse",
+                overview : "The Pulse is the main inciting event of the DragonScape, when an Reality keystone known as The Steiron was shattered in a mining incident the draconic reality of Ohndem was sucked into our own warping the Americas into the DragonScape.", 
+                detailed_info : "",
+                media : "na",
+            },
+            {
+                name : "The Stasis",
+                overview : "After the Pulse the Raddir put the (once human) drekir into a stasis for 3000 years to protect their fallen friends, The Balār. Over these 3000 years of stasis, the cities and signs of humanity would mostly decay, the Sivilāo would settle and establish great realms. Eventually the drekir would awaken into this new, reset world.", 
+                detailed_info : "",
+                media : "na",
+            },
+            {
+                name : "The Awakening",
+                overview : "After 3000 years of a stasis, the Americas of the 21st century would awaken in the reset Americas as drekir. The Awakening Period refers to the first 50 years those first drekir, and the consequent two generations, would experience in the new and mystical Americas", 
+                detailed_info : "",
+                media : "na",
+            },
+            {
+                name : "The Raddir",
+                overview : "The Raddir are the billions of voices held high, those who do not participate in the world yet warp it by their presence and their resonance that rings through reality. They are the soft voices that paint the skies at night", 
+                detailed_info : "",
+                media : "na",
+            },
+        ],
     },
 
-    sivilao : {
-        introduction : "Sivilāo (Pronounced See-veel-yow) is a series of grand societies that were founded across the Americas in the thousands of years prior to the drekir awakening. Founded upon the once standing ruins of old cities across the Americas. Founded on the sacred system of Fim Ēodin, the species stratified caste societies of the sivilāo. As drekir labor and former craft, the mavо̄tur study and the prejag regulates the realm. However, the sivilāo are dogmatic and will not abide by the heretics found in the newer drekir tribes and villages popping up all over the americas.",
+    geography : {
+        overview : "The Dragonscape takes place in the Americas, from Argentina to Greenland and everything inbetween. The Americas have been reset, where once great cities stood now lay expansive old growth forests and magical anomalies and oddities warp the world around them in bizarre and strange ways.",
+        subcategories : [],
     },
 
-    balar : {
-        introduction : "Balār (Pronounced Ball-yar) Are the hundreds of Raddir pulled from the realm of high voices during the Pulse. Forced to establish their life anew, each bal is their own individual situation within the Dragonscape. From the creation of La Tierraniebla by myrmi, the bal GoDomer and it’s cult of worshippers, to Rodurimer shifting the sands of the Mesas, Sila holding the sky isles afloat and Veidrasil hunting the unfortunate the balar some of the more significant aspects of a region.",
+    civilization : {
+        overview : "The cultures that rise from the early chaos after the drekir would awake are varied. The once gleaming modern cultures of the Americas have changed into superstitious and isolated tribal cultures that have adapted to surviving the feral world of the enchanted Americas.",
+        subcategories : [
+            {
+                name : "Fisherdrakes",
+                overview : "aaaaa", 
+                detailed_info : "",
+                media : "na",
+            },
+            {
+                name : "The Falmritians of The Snake River",
+                overview : "aaaaa", 
+                detailed_info : "",
+                media : "na",
+            },
+            {
+                name : "Seattlans",
+                overview : "aaaaa",
+                detailed_info : "",
+                media : "na",
+            },
+            {
+                name : "Masquerade",
+                overview : "aaaaa", 
+                detailed_info : "",
+                media : "na",
+            },
+            {
+                name : "Scraelinger",
+                overview : "aaaaa", 
+                detailed_info : "",
+                media : "na",
+            },
+        ],
     },
 
-    drekir : {
-        introduction : "What became of the Americans? When the pulse hit the peoples of the Americas in the 21st century they were put into a 3000 year stasis, reawakening in the reset Americas as drekir. The drekir are a species of draconic raptor-like beings. Being Vastly different from humans in their physical strengths, social behavior and ergonomics. So the once human drekir of the 1st generation will have to work past their new forms and new lives to carve a new life from the reset American DragonScape.",
-    }
+    magic : {
+        overview : "The DragonScape is awash with magic and mana, influence from the gods and reality warping anomalies. From drekir mages learning to breathe fire to demigod beasts breaking the reality around them magic is alive and pulsing through the Americas.",
+        subcategories : {},
+    },
+
+    flora : {
+        overview : "While less changed overall when compared to the Fauna of the DragonScape, the flora of the dragonscape has changed into enchanted and mystical variants of their prepulse versions. In addition new plants from the reality of О̄ndem have been cultivated and sometimes spread wild through the wildernesses of the DragonScape",
+        subcategories : {},
+    },
+
+    fauna : {
+        overview : "The creatures of the Dragonscape have been warped by the Pulse and the raddir. Many mammals and avians have draconified into the strange reptilian creatures of О̄ndem while others were mutated into entirely new creatures. /nInsects, reptiles, amphibians, and fish have more often mutated into large and more magical creatures",
+        subcategories : {},
+    },
+
+    sentient_species : {
+        overview : "NA",
+        subcategories : [
+            {
+                name : "Balār",
+                pronunciation : "Bal-Yar",
+                overview : "The Balār are fallen Raddir, The voices held high ripped into the mortal realm by the sheer power of the Pulse. Left with no other choice the Balār have settled into their new immortal lives in the Americas. The balār are as different as they are powerful, with each bal shaping the world and warping the reality around them to their whims and interest", 
+                detailed_info : "",
+                media : "balar.png",
+            },
+        ],
+    },
+
+    misc_questions : [
+        {
+            question : "a",
+            answer : [
+                "No",
+                "Bad",
+                "*vaguely frustrated hand gestures*",
+                "You're being put in time out.",
+                "Is this really what you want to be asking?",
+            ],
+            media : "na",
+        },
+        {
+            question : "Can I make my own drekir character/Lore/Stories?",
+            answer : "Yes! Feel free to, I would also encourage you, if you want, to make your own parts of the setting such as your own tribes and creatures, Sivilāo realms and Balār. Make your own stories! Have fun with it!\nIf you want to make something permanent canon in the dragonscape feel free to message Patches with your ideas",
+            media : "na",
+        },
+        {
+            question : "Can one shank a Mavot and live?",
+            answer : "lol no",
+            media : "na",
+        },
+    ],
 }
+
+// loresheet.lore.subcategories.where(n => n = "The Pulse").overview;
